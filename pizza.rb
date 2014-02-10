8679177b49ad0051897245ea02cae67f33914762
Joseph Chappell <jdchappe@gmail.com>
2014-02-10 11:41:32 -0600

    add test for add toppings to pizza, get test to pass

--- pizza.rb
+++ pizza.rb
@@ -1,7 +1,14 @@
 class Pizza
+
+    attr_reader :toppings
+
+    def initialize(toppings)
+      @toppings = toppings
+  end
 end
 
 class Topping
+
   attr_reader :name, :vegetarian
 
   def initialize(topping, vegetarian: false)
